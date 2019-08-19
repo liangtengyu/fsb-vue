49a6697f2c4ae098b4a822f59581844b60e6bd28
lty <srevers@vip.qq.com>
2019-08-19 20:39:10 +0800

    增加样式

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+  <div id="div1"  >
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm" >
 
       <el-form-item label="帐号" prop="input">
       <el-input
@@ -15,16 +15,22 @@
       <el-form-item label="密码" prop="password">
       <el-input placeholder="请输入密码"  prefix-icon="el-icon-key" v-model="ruleForm.password" show-password></el-input>
       </el-form-item>
+      <div id="but">
       <el-switch
         v-model="ruleForm.remeber"
         active-color="#13ce66"
-        inactive-color="#ff4949"
         active-value=true
         inactive-value=false
-        title="记住我">
+        active-text="记住"
+      @change="changeR($event)">
       </el-switch>
 
-      <el-button type="primary" plain icon="el-icon-user" @click="login('ruleForm')">登陆</el-button>
+
+
+      <el-button type="success" class="bt" plain icon="el-icon-thumb" @click="">注册</el-button>
+      <el-button type="primary" class="bt" plain icon="el-icon-user" @click="login('ruleForm')">登陆</el-button>
+
+      </div>
     </el-form>
   </div>
 </template>
@@ -37,7 +43,7 @@
         ruleForm: {
         input: '',
         password: '',
-        remeber: true},
+        remeber: ''},
         rules: {
           input: [
             { required: true, message: '请输入帐号', trigger: 'blur' },
@@ -66,11 +72,11 @@
                 console.log("login success")
                 this.$router.push({path: '/home'})
               }else {
-                alert("登录失败")
+                this.$message.error('登陆失败了');
               }
             }, (response) => {
               console.log(response)
-              alert("登录失败")
+              this.$message.error('登陆失败了');
               // error callback
             });
           } else {
@@ -78,9 +84,34 @@
             return false;
           }
         });
+      },
+      changeR(val){
+        console.log(val)
 
-
-      }
+        if (val){
+          this.$notify.info({
+            title: 'tips',
+            message: '已经记住密码，下次不用输入咯1'
+          });
+        }
     }
   }
+  }
 </script>
+<style>
+  #div1{
+    border-top-width: 20px;
+    padding-right: 20px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    padding-left: 0px;
+    margin-top: 300px;
+  }
+  #but{
+    padding-left: 60px;
+  }
+  .bt{
+
+  }
+
+</style>
